a143140b2514542da81f2fd110b338b7141f3383
nadia-newaz <nadia.nitu55@gmail.com>
2023-03-06 22:02:26 +0600

    complete project

---
+++ index.js
@@ -0,0 +1,14 @@
+// OS, Path modules 
+
+// const os = require("os"); 
+// console.log(os.userInfo());  
+// console.log(os.homedir());  
+// console.log(os.userInfo());         
+// console.log(os.totalmem());         
+// console.log(os.freemem());          
+
+// == Path modules == 
+const path = require("path"); 
+
+const joinNmae = path.join(__dirname + "./views"); 
+console.log(joinNmae);  
